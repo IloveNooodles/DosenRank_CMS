145516f45542b333942c3cbe85ee0afbdaaaed9f
ranjabi <fikriranjabi02@gmail.com>
2023-03-18 14:51:44 +0700

    chore: auto format

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,20 +1,20 @@
 export interface Universities {
-  id: number;
-  name: string;
-  slug: string;
+	id: number;
+	name: string;
+	slug: string;
 }
 
 export interface UniversitiesRowProps extends Universities {
-  action?: VoidFunction;
+	action?: VoidFunction;
 }
 
 export interface Professors {
-  id: number;
-  name: string;
-  institutionId: number;
-  slug: string;
+	id: number;
+	name: string;
+	institutionId: number;
+	slug: string;
 }
 
 export interface ProfessorsRowProps extends Professors {
-  action?: VoidFunction;
+	action?: VoidFunction;
 }
--- src/utils/environment.ts
+++ src/utils/environment.ts
@@ -1,3 +1,4 @@
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://cthehe.site/api/v1';
+export const API_URL =
+	process.env.NEXT_PUBLIC_API_URL || 'https://cthehe.site/api/v1';
 export const APP_API_KEY = process.env.NEXT_PUBLIC_APP_API_KEY;
 export const APP_API_TOKEN = process.env.NEXT_PUBLIC_APP_API_TOKEN;
