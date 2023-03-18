978f908821c8ff191cfcaa9450b2a29e83b1eeaf
ranjabi <fikriranjabi02@gmail.com>
2023-03-18 14:16:04 +0700

    feat: add husky prettier eslint integration

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,26 +1,30 @@
+/* eslint-disable react/prop-types */
 import { useField } from 'formik';
-import { FormControl, FormErrorMessage, FormLabel, Input as ChakraInput } from '@chakra-ui/react';
-import { InputProps as ChakraInputProps } from "@chakra-ui/react";
+import {
+	FormControl,
+	FormErrorMessage,
+	FormLabel,
+	Input as ChakraInput,
+} from '@chakra-ui/react';
+import { InputProps as ChakraInputProps } from '@chakra-ui/react';
 
 interface TextFieldProps extends ChakraInputProps {
-    name: string;
-    label?: string;
-  }
+	name: string;
+	label?: string;
+}
 
 const TextInput: React.FC<TextFieldProps> = ({ label, name, ...props }) => {
-  const [field, meta] = useField(name);
-  
-  return (
-    <FormControl isInvalid={!!(meta.touched && meta.error)}>
-      {label ? <FormLabel htmlFor={props.id}>
-        {label}
-      </FormLabel> : null}
-      <ChakraInput {...field} {...props} bgColor="white" />
-      {meta.touched && meta.error ? (
-         <FormErrorMessage>{meta.error}</FormErrorMessage>
-      ) : null}
-    </FormControl>
-  );
+	const [field, meta] = useField(name);
+
+	return (
+		<FormControl isInvalid={!!(meta.touched && meta.error)}>
+			{label ? <FormLabel htmlFor={props.id}>{label}</FormLabel> : null}
+			<ChakraInput {...field} {...props} bgColor="white" />
+			{meta.touched && meta.error ? (
+				<FormErrorMessage>{meta.error}</FormErrorMessage>
+			) : null}
+		</FormControl>
+	);
 };
 
-export default TextInput
+export default TextInput;
