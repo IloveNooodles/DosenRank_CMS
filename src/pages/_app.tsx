e76c37b9b4ddca84a3ebc9ada31db63657391104
ranjabi <fikriranjabi02@gmail.com>
2023-03-18 12:50:20 +0700

    feat: add login page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,20 @@
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import SidebarWithHeader from "@/components/SidebarWithNav";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
+    const router = useRouter();
+    const currentPath = router.pathname;
+
     return (
         <ChakraProvider>
+            {currentPath === "/login" ? (
+                <Component {...pageProps} />
+            ) : (
             <SidebarWithHeader>
                 <Component {...pageProps} />
-            </SidebarWithHeader>
+            </SidebarWithHeader>)}
         </ChakraProvider>
     );
 }
