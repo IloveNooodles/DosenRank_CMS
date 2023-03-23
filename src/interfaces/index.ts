b4252c04ee69358d983f70fd40d5cca00c466da8
ranjabi <fikriranjabi02@gmail.com>
2023-03-23 10:19:28 +0700

    fix: interface

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,20 +1,61 @@
-export interface Universities {
+import { OptionBase } from 'chakra-react-select';
+import { Props as ReactSelectProps } from 'chakra-react-select';
+
+export interface University {
   id: number;
   name: string;
   slug: string;
 }
 
-export interface UniversitiesRowProps extends Universities {
-  action?: VoidFunction;
+export interface Course {
+  id: number;
+  courseId: number;
+  name: string;
+  univId: number;
+  univName: string;
+  slug: string;
 }
 
-export interface Professors {
+export interface Professor {
   id: number;
   name: string;
   institutionId: number;
+  institutionName: string;
   slug: string;
 }
 
-export interface ProfessorsRowProps extends Professors {
+export interface UniversityRowProps extends University {
   action?: VoidFunction;
+}
+
+export interface ProfessorRowProps extends Professor {
+  action?: VoidFunction;
+}
+
+export interface ProfessorsResponse {
+  data: Professor[];
+}
+export interface ProfessorResponse {
+  data: Professor;
+}
+export interface UniversitiesResponse {
+  data: University[];
+}
+export interface UniversityResponse {
+  data: University;
+}
+
+export interface FormikEditProfessorProps extends Professor {
+  newCourse: number;
+}
+
+export interface SelectOption extends OptionBase {
+  label: string;
+  value: string;
+}
+
+export interface SelectFieldProps extends ReactSelectProps<SelectOption> {
+  name: string;
+  label?: string;
+  options: Array<SelectOption>;
 }
